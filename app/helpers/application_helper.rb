c5f2e6eda3206c9044483cf27529608a9649f47b
danstelle <daniel.stelle@oit.edu>
2016-01-19 00:41:02 +0000

    Added full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  
+  # Changes the title of the page depending on if a title
+  # was given or not
+  def full_title(page_title = '')               # Optional parameter
+    base_title = "CSET Multi-tool Application"  # Base title of the page
+    
+    # If no title was given, just show the base title
+    if page_title.empty?
+      base_title
+    else # Else display the title given with the base title concatenated
+      page_title + " | " + base_title
+    end
+  end
 end
